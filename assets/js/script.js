2f876069e57bcd6d7845957bbd10b62f8b37c37f
Anastasia0877 <anastasiavandysheva58@gmail.com>
2022-12-25 01:00:16 +0100

    price and our-works

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,21 @@ $(document).ready(function(){
         appendArrows:".technology__slider-arrow"
     });
   });
+
+  $('.our-works__main-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    fade: true,
+    asNavFor: '.our-works__second-slider',
+    appendArrows:".our-works__slider-arrow"
+  });
+  $('.our-works__second-slider').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    arrows: false,
+    asNavFor: '.our-works__main-slider',
+    centerMode: true,
+    focusOnSelect: true
+    
+  });
