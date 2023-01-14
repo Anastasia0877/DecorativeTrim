e33908f848d435067cd3e44ec960921b71e22f13
Anastasia0877 <anastasiavandysheva58@gmail.com>
2023-01-14 02:44:39 +0100

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,23 @@
+// burger-menu
+$('.menu-burger').on('click', function(){
+  $('.menu-burger, .menu').toggleClass("active");
+  $('body').toggleClass("overlay");
+})
+
+$(document).click(function(e) {
+  if (!$(e.target).hasClass("active") &&
+      $(e.target).parents(".site-navigation").length === 0) {
+        $('.menu-burger, .menu').removeClass("active");
+        $('body').removeClass("overlay");
+  }
+});
+$('.menu-item').on('click', function(){
+  $('.menu-burger, .menu').removeClass("active");
+  $('body').removeClass("overlay");
+})
+
+
+// button-back-to-top
 function backToTop() {
   var button = $('.back-to-top');
   $(window).on('scroll', () => {
@@ -16,6 +36,7 @@ function backToTop() {
 }
 backToTop();
 
+// sliders
 $(document).ready(function(){
     $('.technology__slider').slick({
         fade: true,
@@ -42,6 +63,7 @@ $(document).ready(function(){
     
   });
 
+  // mask-phone
   $(document).ready(function () {
     $('#phone').mask('+38(000)000-00-00');
 
@@ -52,7 +74,10 @@ var input2 = document.querySelector('input[type=tel]');
 button.addEventListener("click", function () {
     input.value = "";
     input2.value = "";
-    alert('Данні успішно відправлені');
+    // alert('Данні успішно відправлені');
 });
+
+// form
+
 
 
